19f7a5e482dea9c16cc8abc113d9a3ec6621e2bb
feremoreno <feremoreno@yahoo.com>
2024-11-14 16:13:40 -0600

    Actualizacion 1.3

--- planify_fcfm/urls.py
+++ planify_fcfm/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Planify_PIA/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+# Vista para redirigir a la lista de tareas
+def home(request):
+    return redirect('tasks_list')  # Redirige a la vista de lista de tareas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),
+    path('', home),  # Ruta para la raíz redirigiendo a tasks_list
+    path('tasks/', include('tasks.urls')),  # Incluir las URLs de la app tasks
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Task(models.Model):
+    ESTATUS_CHOICES = [
+        (1, 'Vigente'),
+        (0, 'Finalizada'),
+    ]
+
     TAREA_ID = models.AutoField(primary_key=True)
-    ESTATUS = models.IntegerField(default=1)  # 1: vigente, 0: finalizada
+    ESTATUS = models.IntegerField(choices=ESTATUS_CHOICES, default=1)
     TITULO = models.CharField(max_length=50)
     DESCRIPCION = models.CharField(max_length=250, blank=True, null=True)
     FECHA_INICIO = models.DateTimeField(auto_now_add=True)
     FECHA_LIMITE = models.DateTimeField()
+
+    class Meta:
+        db_table = 'TAREAS_CARD'  # Especifica que Django debe usar la tabla existente
 
     def __str__(self):
         return self.TITULO
